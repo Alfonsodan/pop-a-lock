97d2389636db73f5693c13c372f232dfc34517dd
Alfonsodan <danalfons@yahoo.com>
2016-04-17 03:18:19 -0400

    Initial Commit. This is for a class I'm taking. Just fallowing the lecture.... It's a pop a lock game using swift 2.2 and SpriteKit.

--- Pop A Lock/GameScene.swift
+++ Pop A Lock/GameScene.swift
@@ -9,34 +9,43 @@
 import SpriteKit
 
 class GameScene: SKScene {
+    
+    var lock = SKShapeNode()
+    
+    var needle = SKShapeNode()
+    
+    var path = UIBezierPath()
+    
+    let zeroAngle: CGFloat = 0.0
+    
     override func didMoveToView(view: SKView) {
-        /* Setup your scene here */
-        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!"
-        myLabel.fontSize = 45
-        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame))
+        layoutGame()
+     
+    }
+    
+    func layoutGame() {
+    
+        backgroundColor = SKColor.whiteColor()
         
-        self.addChild(myLabel)
+        path = UIBezierPath(arcCenter: CGPoint(x: self.frame.width/2, y: self.frame.height/2), radius: 120, startAngle: zeroAngle, endAngle: zeroAngle + CGFloat(M_PI + 2), clockwise: true)
+        
+        lock = SKShapeNode(path: path.CGPath)
+        lock.strokeColor = SKColor.grayColor()
+        lock.lineWidth = 40.0
+        self.addChild(lock)
+        
+        needle = SKShapeNode(rectOfSize: CGSize(width: 40.0 - 7.0, height: 7.0), cornerRadius: 3.5)
+        needle.fillColor = SKColor.whiteColor()
+        needle.position = CGPoint(x: self.frame.width/2, y: self.frame.height/2 + 120)
+        needle.zRotation = 3.14 / 2
+        needle.zPosition = 2.0
+        self.addChild(needle)
+    
     }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
        /* Called when a touch begins */
-        
-        for touch in touches {
-            let location = touch.locationInNode(self)
-            
-            let sprite = SKSpriteNode(imageNamed:"Spaceship")
-            
-            sprite.xScale = 0.5
-            sprite.yScale = 0.5
-            sprite.position = location
-            
-            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
-            
-            sprite.runAction(SKAction.repeatActionForever(action))
-            
-            self.addChild(sprite)
-        }
+
     }
    
     override func update(currentTime: CFTimeInterval) {
